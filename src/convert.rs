338c9375281321b769f63fb5cb7fbcacea3d603f
irakli mindorashvili <irrum6@github.com>
2024-05-04 01:51:02 +0400

    corrections

--- src/convert.rs
+++ src/convert.rs
@@ -1,9 +1,18 @@
 pub mod convert {
+    //pound force
     pub fn lbf_newtons(force: f64) -> f64 {
         return force * 0.45359237 * 9.8066;
     }
     pub fn newtons_lbf(force: f64) -> f64 {
         return force / (9.8066 * 0.45359237);
+    }
+    //torque pound foot to newtons-meters
+    pub fn lbft_nm(lbft: f64) -> f64 {
+        return lbft * 0.45359237 * 9.8066 * 0.3048;
+    }
+    //torque newton-meters to pound foott
+    pub fn nm_lbft(nm: f64) -> f64 {
+        return nm / (0.45359237 * 9.8066 * 0.3048);
     }
 
     pub fn ftlb_jouls(energy: f64) -> f64 {
--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,14 @@ fn main() {
     use std::env;
     let args: Vec<String> = env::args().collect();
 
-    let version = "0.2.1";
+    const VERSION: &str = "0.2.3";
 
     if args.len() < 2 {
         return;
     }
 
     if args[1] == "--version" || args[1] == "-V" {
-        println!("Volpotorotat v{}", version);
+        println!("Volpotorotat v{}", VERSION);
         return;
     }
 
@@ -31,14 +31,43 @@ fn main() {
         println!("volume is {}", volume);
     }
 
-    if args[1] == "tolbf" {
+    if args[1] == "tolbf" || args[1] == "TL" {
         if args.len() < 3 {
             println!("pass enough parameters to calculate");
             return;
         }
         let force: f64 = args[2].trim().parse().expect("number");
 
-        let lbf = convert::newtons_lbf(force);
-        println!("{} Newtons are equal to {:.5} pounds force", force, lbf);
+        let lbf = convert::nm_lbft(force);
+        println!("{} newton-meters torque are equal to {:.5} pound foots", force, lbf);
+    }
+    //to newton force
+    if args[1] == "tonf" || args[1] == "TN" {
+        if args.len() < 3 {
+            println!("pass enough parameters to calculate");
+            return;
+        }
+        let pound_force: f64 = args[2].trim().parse().expect("number");
+
+        let newtons = convert::lbft_nm(pound_force);
+        println!(
+            "{} pounds foot torque are equal to {:.5} newton-meters ",
+            pound_force, newtons
+        );
+    }
+
+    if args[1] == "pps" {
+        if args.len() < 4 {
+            println!("pass enough parameters to calculate");
+            return;
+        }
+        let torque: f64 = args[2].trim().parse().expect("number");
+        let rpm: u16 = args[3].trim().parse().expect("number");
+
+        let power_ps = tools::power_ps(torque, rpm);
+        println!(
+            "{} Newtons force at {} rpm generates {:.5} horse power(PS)",
+            torque, rpm, power_ps
+        );
     }
 }
